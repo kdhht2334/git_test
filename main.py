22248c00045c4214b1e6b181dccb302a7a0e0e27
Daeha Kim <kdhht5022@gmail.com>
2023-09-13 02:15:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from __future__ import annotations
 import langchain
+print("hello")
 import langsmith
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-print("hello")
+
 aaa = langchain.LLMChain()
 a = torch.zeros((10, 10))
