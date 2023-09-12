89f08abe38f0ba309b02a99860ef464c03e417d0
kdhht2334 <kdhht5022@gmail.com>
2023-09-13 02:11:45 +0900

    Asdf

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from __future__ import annotations
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
+import langchain
 
+aaa = langchain.LLMChain()
 a = torch.zeros((10, 10))
-b = np.zeros((5, 5))
